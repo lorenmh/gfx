3c94ec7c4d6914d4abf14bf22e0e153273371afa
lorenmh <neutronstar@gmail.com>
2019-03-23 00:36:29 -0700

    moving boxes

--- src/View.js
+++ src/View.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as T from 'three';
 import styled from 'styled-components';
+import * as d3 from 'd3'; 
 
 import Cube from './models/Cube';
 
@@ -29,13 +30,13 @@ export default class View extends Component {
     directionalLight.position.set(0, 0, 1000).normalize();
     scene.add(directionalLight);
 
-    const ambientLight = new T.AmbientLight(0xd8d8d8);
+    const ambientLight = new T.AmbientLight(0xa8a8a8);
     scene.add(ambientLight);
 
     const background = Cube({
       width: 1000,
       height: 1000,
-      depth: 1,
+      depth: 10,
       color: 0x8f8f8f8f,
       edges: true,
       x: 0,
@@ -44,7 +45,7 @@ export default class View extends Component {
     });
     scene.add(background);
 
-    const spotLight = new T.SpotLight(0xffffff, 0.1);
+    const spotLight = new T.SpotLight(0xffffff, 0.4);
     spotLight.castShadow = true;
     spotLight.position.set(0, 0, 1000);
     spotLight.angle = Math.PI / 3
@@ -74,9 +75,9 @@ export default class View extends Component {
   populateScene() {
     const { scene }  = this;
 
-    const cube1 = Cube({ color: 0xfa4444, x: 0,    y: 300,   z: 50, edges: false });
-    const cube2 = Cube({ color: 0xfa4444, x: -260, y: -150,  z: 50, edges: false });
-    const cube3 = Cube({ color: 0xfa4444, x: 260,  y: -150,  z: 50, edges: false });
+    const cube1 = Cube({ color: 0xfa4444, x: 0,    y: 300,   z: 60, edges: false });
+    const cube2 = Cube({ color: 0xfa4444, x: -260, y: -150,  z: 60, edges: false });
+    const cube3 = Cube({ color: 0xfa4444, x: 260,  y: -150,  z: 60, edges: false });
 
     const group = new T.Group();
     group.add(cube1, cube2, cube3);
@@ -86,21 +87,32 @@ export default class View extends Component {
     Object.assign(this, { group, cube1, cube2, cube3 });
   }
 
-  animate() {
+  animate(d1=1, d2=1, d3=1) {
     const { group, cube1, cube2, cube3, renderer, scene, camera } = this;
-    requestAnimationFrame(this.animate);
-    group.rotation.z += 0.01;
-    cube1.rotation.z -= 0.005;
-    cube2.rotation.z -= 0.005;
-    cube3.rotation.z -= 0.005;
+    group.position.z += 0.01;
+
+    const dchange = (d, c) => (c.position.z < 60 || c.position.z > 400)
+      ? d * -1
+      : d
+
+    d1 = dchange(d1, cube1);
+    d2 = dchange(d2, cube2);
+    d3 = dchange(d3, cube3);
+
+    cube1.position.z += d1 * 10;
+    cube2.position.z += d2 * 10;
+    cube3.position.z += d3 * 10;
+
     if (camera.position.z < 1000) {
-      camera.position.z += 5;
-      camera.position.y += 5;
-      camera.lookAt(0, 0, 0);
+      //camera.position.z += 5;
+      //camera.position.y += 5;
+      //camera.lookAt(0, 0, 0);
     } else {
     }
 
     renderer.render(scene, camera);
+
+    requestAnimationFrame(this.animate.bind(this, d1, d2, d3));
   }
 
   componentDidMount() {
---
+++ src/models/Ribbon.js
@@ -0,0 +1,5 @@
+import * as T from 'three';
+import * as d3 from 'd3';
+
+export default const Ribbon() {
+}
